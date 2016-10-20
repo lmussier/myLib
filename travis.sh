1a5c2982635a5f87582813aa1fbc5e00bbd7dffe
Ludovic Mussier <ludovic.mussier@non.schneider-electric.com>
2016-10-20 15:59:39 +0200

    Enable travis & coveralls for myLib

---
+++ travis.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+set -evx
+env | sort
+
+mkdir ../build || true
+mkdir ../build/$CMAKE_BUILD_TYPE || true
+cd ../build/$CMAKE_BUILD_TYPE
+cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE ../../myLib
+make && make test
+
+cd -
